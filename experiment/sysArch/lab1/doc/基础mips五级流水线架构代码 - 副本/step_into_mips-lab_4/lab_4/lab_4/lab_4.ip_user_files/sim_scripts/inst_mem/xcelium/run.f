82e1b6bb803c3004b58942d029115a3c92735b5f
spiritTrance <1578154508@qq.com>
2022-10-27 10:15:02 +0800

    20221027

---
+++ experiment/sysArch/lab1/doc/基础mips五级流水线架构代码 - 副本/step_into_mips-lab_4/lab_4/lab_4/lab_4.ip_user_files/sim_scripts/inst_mem/xcelium/run.f
@@ -0,0 +1,10 @@
+-makelib xcelium_lib/xil_defaultlib -sv \
+  "C:/Xilinx/Vivado/2019.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "C:/Xilinx/Vivado/2019.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
